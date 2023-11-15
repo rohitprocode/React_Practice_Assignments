0fc7d1feb87e736368bd81726a2b0c9db4bef046
rohitprocode <rohit990797@gmail.com>
2023-11-15 23:39:29 +0530

    Other Files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import VideoGallery from './Components/VideoGallery';
+
+function App() {
+    return <div className="App">
+    <h1>Welcome to Video Gallery </h1>
+    <VideoGallery/>
+    </div>
+}
+
+export default App;
