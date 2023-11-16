ddebdc22b79f2c7865132fa8b968ce4b420aa232
rohitprocode <rohit990797@gmail.com>
2023-11-17 02:02:40 +0530

    Play Button

---
+++ src/Components/PlayButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PlayButton.css'
+
+function PlayButton() {
+  return (
+      <button onClick={()=>alert("Button Clicked")}>Click Me</button>
+  )
+}
+
+export default PlayButton
