da59d3aac7258c7d14b6d4c8735a3ccbd1289701
rohitprocode <rohit990797@gmail.com>
2023-11-17 01:31:23 +0530

    Refactor Data

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import React from 'react'
 import './App.css'
 import VideoGallery from './Components/VideoGallery'
+import videoData from './data/data'
 
 function App() {
-  let videoData = [{
-    id: 1,
-    title: "React Tutorial in Hindi",
-    channel: 'Coder dost',
-    views: '997K',
-    time: '3 month ago',
-    verified: true
-    },
-    {
-      id:2, 
-      title:"Javascript Tutorial in Hindi",
-      channel:'Coder dost', 
-      views:'540K',
-      time:'1 years ago',
-      verified : false
-    },
-    {
-      id:3, 
-      title:"MongoDB Tutorial in Hindi",
-      channel:'Coder dost', 
-      views:'875K',
-      time:'7 months ago',
-      verified : true
-    }]
-
+ 
   return (
     <div className='App'>
       {
@@ -38,6 +15,7 @@ function App() {
           views= {video.views}
           time= {video.time}
           verified= {video.verified}
+          key={video.id}
         ></VideoGallery>)
       }
       
---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+const videoData = [{
+    id: 1,
+    title: "React Tutorial in Hindi",
+    channel: 'Coder dost',
+    views: '997K',
+    time: '3 month ago',
+    verified: true
+    },
+    {
+      id:2, 
+      title:"Javascript Tutorial in Hindi",
+      channel:'Coder dost', 
+      views:'540K',
+      time:'1 years ago',
+      verified : false
+    },
+    {
+      id:3, 
+      title:"MongoDB Tutorial in Hindi",
+      channel:'Coder dost', 
+      views:'875K',
+      time:'7 months ago',
+      verified : true
+    }]
+
+    export default videoData
