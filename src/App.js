e72fbba13211e47309c8033ecfb7becf968ba724
rohitprocode <rohit990797@gmail.com>
2023-11-17 01:19:54 +0530

    Map in Object

--- src/App.js
+++ src/App.js
@@ -3,22 +3,45 @@ import './App.css'
 import VideoGallery from './Components/VideoGallery'
 
 function App() {
-  let obj = {
-    id:1, 
-    title:"React Tutorial in Hindi",
-    channel:'Coder dost', 
-    views:'540K',
-    time:'3 years ago',
-    verified : true
-  }
+  let videoData = [{
+    id: 1,
+    title: "React Tutorial in Hindi",
+    channel: 'Coder dost',
+    views: '997K',
+    time: '3 month ago',
+    verified: true
+    },
+    {
+      id:2, 
+      title:"Javascript Tutorial in Hindi",
+      channel:'Coder dost', 
+      views:'540K',
+      time:'1 years ago',
+      verified : false
+    },
+    {
+      id:3, 
+      title:"MongoDB Tutorial in Hindi",
+      channel:'Coder dost', 
+      views:'875K',
+      time:'7 months ago',
+      verified : true
+    }]
+
   return (
-    <>
-    <div>
-    <VideoGallery {...obj}/>
-    <VideoGallery id={2} title={"Javascript Tutorial in Hindi"} channel={'Coder dost'} views={'980K'} time={'3 years ago'} verified={false}/>
-    <VideoGallery id={3} title={"Node-Js Tutorial in Hindi"} channel={'Coder dost'} views={'350K'} time={'3 years ago'} verified={true}/>
+    <div className='App'>
+      {
+        videoData.map(video=><VideoGallery
+          id={video.id}
+          title= {video.title}
+          channel= {video.channel}
+          views= {video.views}
+          time= {video.time}
+          verified= {video.verified}
+        ></VideoGallery>)
+      }
+      
     </div>
-    </>
   )
 }
 
